661bfc3d376269486ae291407415de02b2271806
Andrew <andrewsafonovpost@gmail.com>
2025-04-18 13:14:50 +0300

    Added transaction deletion

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("transactions/", views.transactions_list, name='transactions-list'),
     path("transactions/create/", views.create_transaction, name='create-transaction'),
     path("transactions/<int:pk>/update/", views.update_transaction, name='update-transaction'),
+    path("transactions/<int:pk>/delete/", views.delete_transaction, name='delete-transaction'),
 ]
--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 from .models import Transaction
 from .filters import TransactionFilter
@@ -89,3 +90,16 @@ def update_transaction(request, pk):
     }
 
     return render(request, 'tracker/partials/update-transaction.html', context)
+
+
+@login_required
+@require_http_methods(['DELETE'])
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    transaction.delete()
+
+    context = {
+        'message': f'Transaction of {transaction.amount} on {transaction.date} was deleted!'
+    }
+
+    return render(request, 'tracker/partials/transaction-success.html', context)
